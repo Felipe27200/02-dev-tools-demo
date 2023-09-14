e12df27d40ef4a94e7556d995c0fcf5c51604b2f
juan.zea <juan.zea@innclod.com>
2023-09-13 22:54:11 -0500

    Use app properties values

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController
 {
+    // Inject properties for: coach.name and team.name
+
+    /*
+    * We must use the @Value annotation to inject
+    * the values defined in application.properties file.
+    * */
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Expose the value defined for the annotations @Value
+    @GetMapping("/team-info")
+    public String getTeamInfo()
+    {
+        return "Coach: " + this.coachName +
+            ", Team name: " + this.teamName;
+    }
+
     /*
     * Define the route and the data that is going to return.
     *
